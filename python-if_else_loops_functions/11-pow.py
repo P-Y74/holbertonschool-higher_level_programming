f48cc82c2f0ccc7e91786486e34f8f26aa41bae8
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-05-07 14:13:46 +0200

    docs: add docstring to pow function

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 def pow(a, b):
+    """
+    Returns the result of raising a to the power of b.
+
+    Args:
+        a (int or float): The base number.
+        b (int or float): The exponent.
+
+    Returns:
+        int or float: The result of a raised to the power of b.
+    """
     power = a**b
     return power
