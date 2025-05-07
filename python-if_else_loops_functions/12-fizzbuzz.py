22fd474380d5b02b11d3021611539b678b887a1a
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-05-07 14:07:20 +0200

    docs: add docstring to fizzbuzz function

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
+    """
+    Prints numbers from 1 to 100 with substitutions:
+    - 'Fizz' for multiples of 3
+    - 'Buzz' for multiples of 5
+    - 'FizzBuzz' for multiples of both
+    Each value is followed by a space, all on the same line.
+    """
     output = ""
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
