e115c7ea62e04207dc9dc9934d4eee9bf134142c
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-05-07 14:12:48 +0200

    docs: add docstring to add function

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 def add(a, b):
+    """
+    Adds two numbers and returns the result.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The sum of a and b.
+    """
     product = a + b
     return product
