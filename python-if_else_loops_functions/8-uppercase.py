491f7f0361bce02a164fccf471345372d01fe59d
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-05-07 14:11:16 +0200

    docs: add docstring to uppercase function

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def uppercase(str):
+    """
+    Prints a string in uppercase followed by a new line.
+
+    Args:
+        str (str): The input string.
+
+    Notes:
+        - Only two print functions are used.
+        - Does not use str.upper() or any imports.
+    """
     for i in str:
         print("{}".format(chr(ord(i) - 32)
                           if 97 <= ord(i) <= 122 else i), end="")
