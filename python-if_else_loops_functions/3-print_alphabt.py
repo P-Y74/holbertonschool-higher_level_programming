13464fc7055f106a0e53e5bea4dc4515e3f96a45
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-05-07 09:20:54 +0200

    fix: correct the output by indented print function in the for loop

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-alphabet = ""
 for i in range(97, 123):
     if i == 101 or i == 113:
         continue
-    alphabet += chr(i)
-print("{}".format(alphabet), end="")
+    print("{}".format(chr(i)), end="")
