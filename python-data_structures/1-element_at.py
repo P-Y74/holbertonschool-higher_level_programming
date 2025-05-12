c28a61f0e7b1a3fc6c58d44dc57c144bac9d3fe1
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-05-12 10:59:54 +0200

    feat: implement function to retrieve an element from a list by index

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """
+    Retrieve an element from a list at a specific index.
+
+    Args:
+        my_list (list): The list of elements.
+        idx (int): The index of the element to retrieve.
+
+    Returns:
+        The element at the given index, or None if the index is out of range.
+    """
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return(my_list[idx])
