f02162f57a4a6a71ed444587f11062d15eb3d8a3
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-05-15 09:14:05 +0200

    style: reduce line size to respect pycodestyle

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -4,7 +4,8 @@ def raise_exception_msg(message=""):
     Raises a NameError exception with a custom message.
 
     Args:
-        message (str): The message to include in the NameError. Defaults to an empty string.
+        message (str): The message to include in the NameError.
+        Defaults to an empty string.
 
     Raises:
         NameError: Always raised with the provided message.
