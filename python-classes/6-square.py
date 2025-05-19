3f39ad701655f415867170f186ed2e9d43a00123
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-05-19 15:48:01 +0200

    fix: add condition for position in def __init__ and remove private status

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -33,8 +33,11 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        if (not isinstance(position, tuple) or len(position) != 2 or
+                not all(isinstance(i, int) and i >= 0 for i in position)):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__size = size
-        self.__position = position
+        self.position = position
 
     @property
     def size(self):
