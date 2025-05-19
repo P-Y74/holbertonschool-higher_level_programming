121ece5e2add4e576499b6087753691c101ebff1
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-05-19 11:30:38 +0200

    feat: add Square class with __init__ constructor

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+class Square:
+    """A class that defines a square.
+
+    This is an empty class that currently serves as a blueprint
+    for creating Square objects without any attributes or methods.
+    """
+    def __init__(self):
+        """Initializes an instance of the Square class."""
+        pass
