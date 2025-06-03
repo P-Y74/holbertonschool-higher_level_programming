f8dacf6bf3ffc89bae31540312b274cd5585377c
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-06-03 09:49:42 +0200

    style: reduce size line to respect pycodestyle

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -10,14 +10,17 @@ it is created. If it does exist, its content is overwritten.
 
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file (UTF-8) and returns the number of characters written.
+    Writes a string to a text file (UTF-8) and returns the number of
+    characters written.
 
-    This function uses the with statement for file handling. It creates the file
-    if it doesn't exist and overwrites it if it does.
+    This function uses the with statement for file handling. It creates
+    the file if it doesn't exist and overwrites it if it does.
 
     Args:
-        filename (str): The name of the file to write to. Defaults to an empty string.
-        text (str): The string to write into the file. Defaults to an empty string.
+        filename (str): The name of the file to write to. Defaults to
+        an empty string.
+        text (str): The string to write into the file. Defaults to
+        an empty string.
 
     Returns:
         int: The number of characters written to the file.
