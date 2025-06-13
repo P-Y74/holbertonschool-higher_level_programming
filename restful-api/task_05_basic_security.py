19d30ec07c0c3184026f4623badeb2513c611a1a
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-06-13 14:41:38 +0200

    fix(auth): align admin key with username to pass basic auth

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -43,7 +43,7 @@ jwt = JWTManager(app)
 users = {
     "user1": {"username": "user1",
               "password": generate_password_hash("password"), "role": "user"},
-    "admin1": {"username": "admin",
+    "admin": {"username": "admin",
                "password": generate_password_hash("password"), "role": "admin"}
 }
 
