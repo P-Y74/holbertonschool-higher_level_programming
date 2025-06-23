b0ce5baf549b95f8d27ffa3bec81baf34ee30c5e
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-06-23 16:40:54 +0200

    fix: delete score from SELECT

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- List the number of records with the same score in the table second_table of the database hbtn_0c_0
-SELECT score, COUNT(*) AS number
+SELECT COUNT(*) AS number
 FROM second_table
 GROUP BY score;
