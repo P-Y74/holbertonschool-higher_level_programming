ee1330a575d71df57f9286cb6ae3f11a67d40929
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-06-26 14:18:27 +0200

    chore: correct BINARY place

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     )
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
