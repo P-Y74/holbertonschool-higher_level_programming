02691e4b1239846b01b4c4002001f480cb3d773c
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-06-27 17:04:25 +0200

    feat: add script to fetch and display cities with their state names ordered by city id

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,41 @@
+#!/user/bin/python3
+"""
+Script that prints all City objects from the database `hbtn_0e_14_usa`.
+
+Usage:
+    ./14-model_city_fetch_by_state.py <username> <password> <database>
+
+The script:
+- Connects to a MySQL server on localhost.
+- Joins City and State tables using SQLAlchemy ORM.
+- Displays cities sorted by city.id in the format: <state name>:
+(<city id>) <city name>
+"""
+import sys
+
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)  # type: ignore
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City, State).join(
+        State).order_by(City.id).all()  # type: ignore
+
+    for city, state in cities:
+        print(f"{state.name}: ({city.id}) {city.name}")
+
+    session.close()
---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Defines the City class mapped to the `cities` table using SQLAlchemy ORM.
+
+This module requires the `Base` class from `model_state` and creates a City
+class with attributes corresponding to table columns.
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """City model for the `cities` table in the MySQL database.
+
+    This class represents a city and is mapped to the `cities` table
+    using SQLAlchemy ORM. It includes an auto-incremented ID, a name,
+    and a foreign key reference to the `states` table.
+
+    Attributes:
+        id (int): Primary key. Auto-generated, unique, non-nullable.
+        name (str): Name of the city. Max length 128 characters. Non-nullable.
+        state_id (int): Foreign key referencing `states.id`. Non-nullable.
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
