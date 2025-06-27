59c4537534b51a49aeb8067412563d6ff3e94532
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-06-27 09:50:21 +0200

    feat(models): define State model with SQLAlchemy ORM

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module defines a `State` class that represents a row in the `states`
+table.
+
+It uses SQLAlchemy's Object Relational Mapping (ORM) to map the class
+to a MySQL table. This class inherits from the SQLAlchemy declarative base.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    A class that represents a state in the MySQL database.
+
+    Attributes:
+        __tablename__ (str): Name of the table in the database.
+        id (int): Primary key, auto-incremented, cannot be null.
+        name (str): Name of the state, string up to 128 characters,
+        cannot be null.
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
