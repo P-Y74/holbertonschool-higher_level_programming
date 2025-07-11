52fd4c086f1224d4bff4caaa80be576f2f15043a
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-07-11 10:31:29 +0200

    style: fix semistandard style issues

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 function factorial (n) {
-  if (isNaN(arg) || n == 0 || n == 1) {
+  if (isNaN(arg) || n === 0 || n === 1) {
     return 1;
   }
   return n * factorial(n - 1);
 }
 
 const arg = parseInt(process.argv[2]);
-let result = factorial(arg)
+const result = factorial(arg);
 
 console.log(result);
