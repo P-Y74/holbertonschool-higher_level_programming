87b3fe1edd86e07cec0aa5f8f99b5a0332343799
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-07-11 09:34:09 +0200

    feat: add script that print x times 'C is fun'

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+const toPrint = 'C is fun';
+if (isNaN(arg)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < arg; i++) {
+    console.log(toPrint);
+  }
+}
